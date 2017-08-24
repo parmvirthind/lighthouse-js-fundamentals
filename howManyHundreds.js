c3126e462ffd0e1d7003d33480426d6b855dbfa2
Parmvir <parmvirsthind@gmail.com>
2017-08-24 22:23:00 +0000

    Adds conditionals exercises

---
+++ howManyHundreds.js
@@ -0,0 +1,10 @@
+function howManyHundreds(number) {
+  var remainder = number % 100;
+  return (number - remainder) / 100;
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
